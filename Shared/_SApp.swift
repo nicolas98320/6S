2b41c8404f90d57ba3a1acae2d895e33952dbeb7
ek <eksaraste@icloud.com>
2021-10-14 12:58:45 +1100

    Empty project

---
+++ Shared/_SApp.swift
@@ -0,0 +1,17 @@
+//
+//  _SApp.swift
+//  Shared
+//
+//  Created by Ukko on 14/10/21.
+//
+
+import SwiftUI
+
+@main
+struct _SApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
